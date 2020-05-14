d9a062bb89009ecf0c7546462d006389d946674d
PIN-devel <pin954562@gmail.com>
2020-05-14 23:18:41 +0900

    make user model and admin page

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import User
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['id','username','last_name','first_name','email']
+    list_display_links = ['username']
